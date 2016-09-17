ff145b5feba742c0f461c943f65ca435256aee1d
rush0714 <pukanGi9>
2016-09-18 00:49:27 +0300

    comment

--- src/main/java/com/no0ker/configs/EndpointConfig.java
+++ src/main/java/com/no0ker/configs/EndpointConfig.java
@@ -5,35 +5,48 @@ import com.no0ker.domain.SetEventResponse;
 import com.no0ker.model.Event;
 import com.no0ker.model.EventDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import javax.transaction.TransactionManager;
+
 @Endpoint
-public class EndpointConfig
-{
+public class EndpointConfig {
     private static final String NAMESPACE_URI = "https://github.com/no0ker/MyExampleVer2Application";
 
-//    private DbHelper dbHelper;
+    //    private DbHelper dbHelper;
+    @Autowired
     private EventDAO eventDAO;
 
     @Autowired
-    public EndpointConfig(EventDAO eventDAO) {
-        this.eventDAO = eventDAO;
-    }
+    private HibernateTransactionManager transactionManager;
+
+//    public EndpointConfig(EventDAO eventDAO) {
+//        this.eventDAO = eventDAO;
+//    }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "setEventRequest")
     @ResponsePayload
-    public SetEventResponse eventsProcessor(@RequestPayload SetEventRequest request)
-    {
+    public SetEventResponse eventsProcessor(@RequestPayload SetEventRequest request) {
 
         SetEventResponse response = new SetEventResponse();
         response.setName(request.getName());
         Event event = new Event();
         event.setName(request.getName());
-        event.setId(1);
-        eventDAO.save(event);
+
+        new TransactionTemplate(transactionManager).execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus transactionStatus) {
+                eventDAO.save(event);
+                return null;
+            }
+        });
 //
 //        try{
 //            dbHelper.saveEvent(eventName);
--- src/main/java/com/no0ker/model/EventDAO.java
+++ src/main/java/com/no0ker/model/EventDAO.java
@@ -3,14 +3,16 @@ package com.no0ker.model;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class EventDAO {
 
     @Autowired
     SessionFactory sessionFactory;
 
     public void save(Event event){
-        sessionFactory.openSession().save(event);
+        sessionFactory.getCurrentSession().save(event);
     }
 }
